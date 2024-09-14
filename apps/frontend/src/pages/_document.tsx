0666d5a6ea979ec9a5c3bfd5cf57084f044423ea
Henry Perkins <htperkins@gmail.com>
2024-09-14 05:23:10 -0500

    Update _document.tsx

---
+++ apps/frontend/src/pages/_document.tsx
@@ -0,0 +1,20 @@
+// apps/frontend/src/pages/_document.tsx
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Add meta tags, fonts, etc. */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
