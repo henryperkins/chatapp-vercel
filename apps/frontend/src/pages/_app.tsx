72d561ec1be9302a3a95e001ea9a1db2427b00cf
Henry Perkins <htperkins@gmail.com>
2024-09-14 16:11:20 -0500

    s

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -8,8 +8,14 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false; // Prevent Font Awesome from adding its CSS automatically
 
+import { AuthProvider } from '../context/AuthContext';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 };
 
 export default MyApp;
