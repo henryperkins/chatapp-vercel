934a0a8bfe3771c263a02e3500a9474648200fae
Henry Perkins <htperkins@gmail.com>
2024-09-13 21:48:23 -0500

    Create cors.ts

---
+++ apps/backend/utility/middleware/cors.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import NextCors from 'nextjs-cors';
+
+export async function cors(req: NextApiRequest, res: NextApiResponse) {
+  await NextCors(req, res, {
+    origin: (process.env.ALLOWED_ORIGINS || '*').split(','),
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    optionsSuccessStatus: 200,
+  });
+}
